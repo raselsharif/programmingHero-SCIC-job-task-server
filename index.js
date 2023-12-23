485d59b1636c9be36d7c28215c2c6701f7b7214a
raselsharif <raselsharif9@gmail.com>
2023-12-23 16:02:58 +0600

    create api for get task by user email, task update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -36,6 +36,41 @@ async function run() {
     app.get("/tasks", async (req, res) => {
       const tasks = await taskCollection.find().toArray();
       res.send(tasks);
+    });
+    app.get("/task/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await taskCollection.find(filter).toArray();
+      res.send(result);
+    });
+    app.get("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(filter);
+      res.send(result);
+    });
+    app.put("/task/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const task = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateTask = {
+        $set: {
+          ...task,
+        },
+      };
+      const result = await taskCollection.updateOne(
+        filter,
+        updateTask,
+        options
+      );
+      res.send(result);
+    });
+    app.delete("/task/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(filter);
+      res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
